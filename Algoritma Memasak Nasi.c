8ea0c8933792d22fa7b3439581a3a53d2551fe76
Aguskay <32824557+aguskay@users.noreply.github.com>
2017-12-01 10:09:50 +0700

    Add files via upload

---
+++ Algoritma Memasak Nasi.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+	int main () {
+	printf ("1. masukan beras ke dalam wadah \n");
+	printf ("2. cuci beras hingga bersih \n");
+	printf ("3. masukan wadah berisi beras ke dalam magicom \n");
+	printf ("4. nyalakan magicom dan tunggu selama 30 menit \n");
+	printf ("5. nasi siap di hidangkan \n");
+	return 0;
+
+	}
---
+++ Hello STTB.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main()
+{
+   printf("Hello STTB");
+   return 0;
+}
